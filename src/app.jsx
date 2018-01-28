6353854e12ce41d582b37b21c1b10df7ea133c34
Bart Glogowski <bglogowski@ws50639.home>
2018-01-28 21:56:41 +0000

    Initial commit

---
+++ src/app.jsx
@@ -0,0 +1,7 @@
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
+import 'normalize.css/normalize.css';
+import './styles/styles.scss';
+
+ReactDOM.render(<p>This is just the beggining</p>, document.getElementById('app'));
