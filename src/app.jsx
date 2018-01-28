6fc151a1ab6557020658c8f3d8631dc701cb3a72
Bart Glogowski <bglogowski@ws50639.home>
2018-01-28 22:53:11 +0000

    A little bit fun with react-router

--- src/app.jsx
+++ src/app.jsx
@@ -1,7 +1,49 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
-ReactDOM.render(<p>This is just the beggining</p>, document.getElementById('app'));
+const ExpenseDashboardPage = () => (
+  <div>
+    This is dashboard component
+  </div>
+);
+
+const AddExpensePage = () => (
+  <div>
+    This is add expense component
+  </div>
+);
+
+const EditExpensePage = () => (
+  <div>
+    This is add edit expense component
+  </div>
+);
+
+const HelpPage = () => (
+  <div>
+    This is help component
+  </div>
+);
+
+const NotFoundPage = () => (
+  <div>
+    404!
+  </div>
+);
+
+const routes = (
+  <BrowserRouter>
+    <Switch>
+      <Route path={'/'} component={ExpenseDashboardPage} exact/>
+      <Route path={'/create'} component={AddExpensePage}/>
+      <Route path={'/edit'} component={EditExpensePage}/>
+      <Route path={'/help'} component={HelpPage}/>
+      <Route component={NotFoundPage}/>
+    </Switch>
+  </BrowserRouter>
+);
+
+ReactDOM.render(routes, document.getElementById('app'));
