030305e09cde46dcd66f10219fe5186febd3a682
Bart Glogowski <bglogowski@MrG.local>
2018-02-05 14:41:10 +0000

    Updated expense list item and tests

--- src/components/ExpenseListItem.jsx
+++ src/components/ExpenseListItem.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import moment from 'moment';
+import numeral from 'numeral';
 
 const ExpenseListItem = ({id, description, amount, note, createdAt, dispatch}) => (
   <div>
     <Link to={`/edit/${id}`}> <p>Description: {description}</p> </Link>
-    <p>Amount: {amount / 100} GBP</p>
+    <p>Amount: {numeral(amount/100).format('$0,0.00')} GBP</p>
     {note && <p>Note: {note}</p>}
-    <p>Created at: {createdAt}</p>
+    <p>Created at: {moment(createdAt).format('MMMM Do, YYYY')}</p>
   </div>
 
 );
