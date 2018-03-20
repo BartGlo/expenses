6662571ad62f7be71d0fe5d6fab2cc29c2a8476c
MrG <glogowski.bart@gmail.com>
2018-03-20 19:16:36 +0000

    Refactor

--- src/app.jsx
+++ src/app.jsx
@@ -1,16 +1,16 @@
-import React, {Component} from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import AppRouter, {history} from './routers/AppRouter';
-import configureStore from './store/configureStore';
-import { startSetExpenses } from './actions/expenses';
-import { login, logout } from './actions/auth';
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import AppRouter, { history } from "./routers/AppRouter";
+import configureStore from "./store/configureStore";
+import { startSetExpenses } from "./actions/expenses";
+import { login, logout } from "./actions/auth";
 // import { setTextFilter } from './actions/filters';
-import getVisibleExpenses from './selectors/expenses';
-import 'normalize.css/normalize.css';
-import './styles/styles.scss';
-import 'react-dates/lib/css/_datepicker.css';
-import {firebase} from './firebase/firebase';
+import getVisibleExpenses from "./selectors/expenses";
+import "normalize.css/normalize.css";
+import "./styles/styles.scss";
+import "react-dates/lib/css/_datepicker.css";
+import { firebase } from "./firebase/firebase";
 
 const store = configureStore();
 
@@ -22,29 +22,27 @@ const jsx = (
 
 let hasRendered = false;
 const renderApp = () => {
-  if(!hasRendered) {
-    ReactDOM.render(jsx, document.getElementById('app'));
+  if (!hasRendered) {
+    ReactDOM.render(jsx, document.getElementById("app"));
     hasRendered = true;
   }
 };
 
-ReactDOM.render(<p>Loading...</p>, document.getElementById('app'));
+ReactDOM.render(<p>Loading...</p>, document.getElementById("app"));
 
-
-
-firebase.auth().onAuthStateChanged((user) => {
-  if(user) {
+firebase.auth().onAuthStateChanged(user => {
+  if (user) {
     store.dispatch(login(user.uid));
-    store.dispatch(startSetExpenses()).then(()=>{
+    store.dispatch(startSetExpenses()).then(() => {
       renderApp();
-      if(history.location.pathname === '/') {
-        history.push('/dashboard')
+      if (history.location.pathname === "/") {
+        history.push("/dashboard");
       }
     });
   } else {
     store.dispatch(logout());
     renderApp();
-    history.push('/');
+    history.push("/");
   }
 });
 
@@ -65,7 +63,6 @@ firebase.auth().onAuthStateChanged((user) => {
 // setTimeout(()=> {
 //   store.dispatch(setTextFilter('bill'));
 // }, 3000);
-
 
 // const state = store.getState();
 // const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
